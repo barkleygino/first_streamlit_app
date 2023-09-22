72f430c7725533d6995e2689568804beb658493c
barkleygino <89505661+barkleygino@users.noreply.github.com>
2023-09-22 10:36:30 +0200

    Update streamlit_app.py
    Putting multiselect in correct place

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,6 @@
 import streamlit
 import pandas
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
-# Display the table on the page.
-
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -14,4 +9,10 @@ streamlit.text('🥚Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🍊Build your own Smoothie🍇🥝')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
+
 streamlit.dataframe(my_fruit_list)
